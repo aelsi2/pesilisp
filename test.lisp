bbe98b50f00b6b56cd198ca67afc1666a165e6dd
aelsi2 <eliseevandrey2005@gmail.com>
2025-05-27 01:03:59 +0700

    add test funcs

--- test.lisp
+++ test.lisp
@@ -1,1 +1,2 @@
-(quit 5)
+(defun fib (num) (if (= num 0) 0 (if (= num 1) 1 (+ (fib (- num 2)) (fib (- num 1))))))
+(defun pascal (n k) (if (or (= n k) (= k 0)) 1 (+ (pascal (- n 1) (- k 1)) (pascal (- n 1) k))))
