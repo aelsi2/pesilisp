370e4f7d0d5713e225eb6b01ba3d960d79eec651
Andrey Eliseev <eliseevandrey2005@gmail.com>
2025-06-11 00:09:11 +0700

    add console color support for windows

--- src/tty.c
+++ src/tty.c
@@ -1,28 +1,58 @@
+#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
+#define TTY_UNIX
+#elif defined(_WIN32)
+#define TTY_WINDOWS
+#endif
+
 #include "tty.h"
 #include <stdbool.h>
-#ifdef linux
+
+#ifdef TTY_UNIX
 #include <unistd.h>
+#elifdef TTY_WINDOWS
+#include <windows.h>
 #endif
 
 #define ANSI_COLOR "\x1b[%dm"
 
-static bool is_tty(FILE *file) {
-#ifdef linux
-    return isatty(fileno(file));
-#else
-    return false;
-#endif
-}
-
 void begin_color(FILE *file, int color) {
-    if (is_tty(file)) {
+#ifdef TTY_UNIX
+    if (isatty(fileno(file))) {
         fprintf(file, ANSI_COLOR, color);
     }
+#elifdef TTY_WINDOWS
+    WORD attributes;
+    switch (color) {
+        case COLOR_RED:
+            attributes = FOREGROUND_RED;
+            break;
+        case COLOR_GREEN:
+            attributes = FOREGROUND_GREEN;
+            break;
+        case COLOR_BLUE:
+            attributes = FOREGROUND_BLUE;
+            break;
+    }
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    if (GetConsoleMode(handle, &mode)) {
+        SetConsoleTextAttribute(handle, attributes);
+    }
+#endif
 }
 
 
 void end_color(FILE *file) {
-    if (is_tty(file)) {
+#ifdef TTY_UNIX
+    if (isatty(fileno(file))) {
         fprintf(file, ANSI_COLOR, 0);
     }
+#elifdef TTY_WINDOWS
+    WORD attributes = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    if (GetConsoleMode(handle, &mode)) {
+        SetConsoleTextAttribute(handle, attributes);
+    }
+#endif
 }
